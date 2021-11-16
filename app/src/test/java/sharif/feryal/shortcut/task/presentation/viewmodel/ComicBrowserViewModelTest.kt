e4d9a770c6602588bc7e4dc28f4c087e5693475d
Feryal <feryal.sharifzadeh@tap30co.com>
2021-11-16 10:43:28 +0330

    Add some failed tests for next, and previous calls

--- app/src/test/java/sharif/feryal/shortcut/task/presentation/viewmodel/ComicBrowserViewModelTest.kt
+++ app/src/test/java/sharif/feryal/shortcut/task/presentation/viewmodel/ComicBrowserViewModelTest.kt
@@ -3,7 +3,8 @@ package sharif.feryal.shortcut.task.presentation.viewmodel
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
-import io.mockk.impl.annotations.MockK
+import io.mockk.coVerify
+import io.mockk.impl.annotations.RelaxedMockK
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.test.runBlockingTest
@@ -28,10 +29,10 @@ class ComicBrowserViewModelTest {
     @get:Rule
     val coroutineTestRule = CoroutineTestRule()
 
-    @MockK
+    @RelaxedMockK
     private lateinit var comicUseCase: GetComicUseCase
 
-    private val anyNumber = 0
+    private val anyNumber = 100
     private val anyDate = Date("", "", "")
     private val mockComic = Comic(
         anyNumber,
@@ -52,6 +53,19 @@ class ComicBrowserViewModelTest {
 
     private val viewModel: ComicBrowserViewModel by lazy {
         ComicBrowserViewModel(comicUseCase)
+    }
+
+    private fun simulateSettingCurrentComic(currentComicNumber: Int, maxNumber: Int) {
+        coEvery { comicUseCase.getComic(any()) } returnsMany listOf(
+            // First api call will set comics max number
+            Either.Success(mockComic.copy(number = maxNumber)),
+            // any second api call will return this currentComicNumber as its number(it's just
+            // for mocking)
+            Either.Success(mockComic.copy(number = currentComicNumber))
+        )
+
+        // We couldn't call next here but at first next is disable
+        viewModel.previousComicRequested()
     }
 
     @Test
@@ -81,5 +95,124 @@ class ComicBrowserViewModelTest {
         coEvery { comicUseCase.getComic() } coAnswers { successResponse }
 
         assertEquals(mockComic, viewModel.comic.value)
+    }
+
+    @Test
+    fun `when previous Comic is requested, then data-status should be loading`() {
+        coEvery { comicUseCase.getComic() } coAnswers {
+            Either.Success(mockComic)
+        }
+        val prevNumber = mockComic.number - 1
+        coEvery { comicUseCase.getComic(prevNumber) } coAnswers {
+            delay(500)
+            val previousComic = mockComic.copy(prevNumber)
+            Either.Success(previousComic)
+        }
+
+        viewModel.previousComicRequested()
+
+        assert(viewModel.dataStatus.value is DataStatus.Loading)
+    }
+
+    @Test
+    fun `when previous Comic is requested, and it failed, then data-status should be failed`() {
+        coEvery { comicUseCase.getComic() } coAnswers {
+            Either.Success(mockComic)
+        }
+        val prevNumber = mockComic.number - 1
+        val error = Throwable()
+        coEvery { comicUseCase.getComic(prevNumber) } coAnswers {
+            Either.Failure(error)
+        }
+
+        viewModel.previousComicRequested()
+
+        assert(viewModel.dataStatus.value is DataStatus.Failed)
+    }
+
+    @Test
+    fun `when get previous Comic successfully, then Comic live-data should be updated`() {
+        coEvery { comicUseCase.getComic() } coAnswers {
+            Either.Success(mockComic)
+        }
+        val prevNumber = mockComic.number - 1
+        val previousComic = mockComic.copy(prevNumber)
+        coEvery { comicUseCase.getComic(prevNumber) } coAnswers {
+            Either.Success(previousComic)
+        }
+
+        viewModel.previousComicRequested()
+
+        assertEquals(previousComic, viewModel.comic.value)
+    }
+
+    @Test
+    fun `when it's already the first comic, then request previous comic should not call api`() {
+        val firstComicNum = 0
+        coEvery { comicUseCase.getComic() } coAnswers {
+            Either.Success(mockComic.copy(number = firstComicNum))
+        }
+
+        viewModel.previousComicRequested()
+
+        coVerify(timeout = 0) { comicUseCase.getComic(any()) }
+    }
+
+    @Test
+    fun `when next Comic is requested, then data-status should be loading`() {
+        val currentNumber = 2
+        simulateSettingCurrentComic(currentComicNumber = currentNumber, maxNumber = 5)
+        val nextComic = mockComic.copy(number = currentNumber + 1)
+        coEvery { comicUseCase.getComic(any()) } coAnswers {
+            delay(500)
+            Either.Success(nextComic)
+        }
+
+        viewModel.nextComicRequested()
+
+        assert(viewModel.dataStatus.value is DataStatus.Loading)
+    }
+
+    @Test
+    fun `when next Comic is requested, and it failed, then data-status should be failed`() {
+        val currentNumber = 2
+        simulateSettingCurrentComic(currentComicNumber = currentNumber, maxNumber = 5)
+        val nextComic = mockComic.copy(number = currentNumber + 1)
+        val error = Throwable()
+        coEvery { comicUseCase.getComic(any()) } coAnswers {
+            Either.Failure(error)
+        }
+
+        viewModel.nextComicRequested()
+
+        assert(viewModel.dataStatus.value is DataStatus.Failed)
+    }
+
+    @Test
+    fun `when get next Comic successfully, then Comic live-data should be updated`() {
+        val currentNumber = 2
+        simulateSettingCurrentComic(currentComicNumber = currentNumber, maxNumber = 5)
+        val nextComic = mockComic.copy(number = currentNumber + 1)
+        coEvery { comicUseCase.getComic(any()) } coAnswers {
+            Either.Success(nextComic)
+        }
+
+        viewModel.nextComicRequested()
+
+        assertEquals(nextComic, viewModel.comic.value)
+    }
+
+    @Test
+    fun `when it's already the last comic, then request next comic should not call api`() {
+        val currentNumber = 5
+        simulateSettingCurrentComic(currentComicNumber = currentNumber, maxNumber = currentNumber)
+        val nextComic = mockComic.copy(number = currentNumber + 1)
+        coEvery { comicUseCase.getComic(any()) } coAnswers {
+            Either.Success(nextComic)
+        }
+
+        viewModel.nextComicRequested()
+
+        coVerify(timeout = 0) { comicUseCase.getComic(any()) }
     }
 }
